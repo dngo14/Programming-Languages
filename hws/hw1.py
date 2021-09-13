c6ac61be1fe11a4a632ab2d1e533c7aa0cccfb06
Daniel <ngo8@stolaf.edu>
2021-09-12 23:28:15 -0500

    hw1

---
+++ hws/hw1.py
@@ -0,0 +1,32 @@
+print("hello")
+x = 1
+y = x + 2
+print(y)
+z = "y\'all are weird\n"
+print(z)
+k = "No"
+print(k+" Thanks")
+print(k[0])
+l = "hello my name is no"
+print(l[0:5])
+print(l[7:])
+print(len(l))
+lists = [1,2,3,4,5]
+print(lists)
+lists[2] = 5
+print(lists)
+lists.append(100)
+print(lists)
+a = 0
+while a < len(l):
+    print(l[a]+".")
+    a = a+1
+
+
+#Project Ideas
+#Rock Paper Scissors Game
+#Text Adventure Game
+#Calculator
+#Speed Typing Game
+#Webscraper
+#App to display breakfast/lunch/dinner menu
