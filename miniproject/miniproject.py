86c029c1e9dc33fec635aab81436829eafd105a3
Daniel <ngo8@stolaf.edu>
2021-09-20 19:44:00 -0500

    updates to mini project

--- miniproject/miniproject.py
+++ miniproject/miniproject.py
@@ -1,26 +1,30 @@
 #imports
 import time
 import random
+import threading
 
 def prompts(): #function to choose a randome prompt
-    prompt1 ="I had never seen a house on fire before, so, one evening when I heard fire engines"\
-    #"with loud alarm bells rushing past my house. I quickly ran out and, a few streets away, joined a large"\
-    #"crowd of people. We could see the fire only from a distance because the police would not allow any one near the building on fire"\
-    #"What a terrible scene I saw that day! Huge flames of fire were coming out of each floor, and black and thick smoke spread all around."\
-    #"Four fire engines were engaged and the firemen in their uniform were playing the hose on various parts of the building."\
-    #"Then the tall ladders of the fire engine were stretched upwards. Some firemen climbed up with hoses in their hands."\
-    #"The continuous flooding brought the fire under control but the building was completely destroyed."
+    prompt1 = "I had never seen a house on fire before, so, one evening when I heard fire engines, I ran towards the sound"
+ 
+    prompt2 = "Last month a grand exhibition was held in our city. My friends and I went to see it in the evening."
 
-    prompt2 = "Last month a grand exhibition was held in our city. My friends and I went to see it in evening."\
-    #"Our first impression on entering the grounds was that whole thing looked like a fairyland. The vast space was"\
-    #"decorated in magnificent, bright and purple colour and lit up with countless lights. Men, women and children were"\
-    #"moving from corner to corner, admiring the beauty of all kinds of stalls set up. These stalls were like small shops."\
-    #"While the stalls made a very interesting sight, what attracted us most was the Children's Corner in the exhibition."\
-    #"The Children's Corner was crowded with boys and girls. All types of amusements could be seen here. Children and some grown-ups"\
-    #"were enjoying the giant wheel, wooden hoses, dodge-cars, railway train and other things. I too had my share of fun with my friends"\
-    #"and returned home after enjoying a most delightful evening."
+    prompt3 = "He is no James Bond; his name is Roger Moore. The irony of the situation wasn't lost on anyone in the room."
 
-    listofprompts = [prompt1, prompt2]
+    prompt4 = "The lone lamp post of the one-street town flickered, not quite dead but definitely on its way out. Suitcase by her side, she paid no heed to the light, the street or the town. A car was coming down the street and with her arm outstretched and thumb in the air, she had a plan."
+
+    prompt5 = "She sat in the darkened room waiting. It was now a standoff. He had the power to put her in the room, but not the power to make her repent. It wasn't fair and no matter how long she had to endure the darkness, she wouldn't change her attitude. At three years old, Sandy's stubborn personality had already bloomed into full view."
+
+    prompt6 = "There are only three ways to make this work. The first is to let me take care of everything. The second is for you to take care of everything. The third is to split everything 50 / 50. I think the last option is the most preferable, but I'm certain it'll also mean the end of our marriage."
+
+    prompt7 = "I haven't bailed on writing. Look, I'm generating a random paragraph at this very moment in an attempt to get my writing back on track. I am making an effort. I will start writing consistently again!"
+
+    prompt8 = "She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?"
+
+    prompt9 = "She considered the birds to be her friends. She'd put out food for them each morning and then she'd watch as they came to the feeders to gorge themselves for the day. She wondered what they would do if something ever happened to her. Would they miss the meals she provided if she failed to put out the food one morning?"
+
+    prompt10 = "There was no time. He ran out of the door without half the stuff he needed for work, but it didn't matter. He was late and if he didn't make this meeting on time, someone's life may be in danger."
+
+    listofprompts = [prompt1, prompt2, prompt3, prompt4, prompt5, prompt6, prompt7, prompt8, prompt9, prompt10]
     x = random.choice(listofprompts)
 
     return x
@@ -28,13 +32,28 @@ def prompts(): #function to choose a randome prompt
 def finderrors(prompt, userinput): #function to find errors
     promptwords = prompt.split()
     userwords = userinput.split()
-
-    listofdifferences = list(set(promptwords).difference(userwords))
-    errors = len(listofdifferences)
-    print(listofdifferences)
-
-    listofcommon = list(set(promptwords).intersection(userwords))
-    correctentries = len(listofcommon)
+    errors = 0
+    # listofdifferences = list(set(promptwords).difference(userwords))
+    # errors = len(listofdifferences)
+    # print(listofdifferences)
+    #listofcommon = list(set(promptwords).intersection(userwords))
+    correctentries = 0
+    for index in range(len(userwords)):
+        if index in (0, len(userwords)-1):
+            if userwords[index] == promptwords[index]:
+                correctentries+=1
+                continue
+            else:
+                errors+=1
+        else:
+            if userwords[index] == promptwords[index]:
+                if (userwords[index+1] == promptwords[index+1]) & (userwords[index-1] == promptwords[index-1]):
+                    correctentries+=1
+                    continue
+                else:
+                    errors+=1
+            else:
+                errors+=1
 
     promptspaces = promptwords.count(" ")
     userspaces = userwords.count(" ")
@@ -44,8 +63,6 @@ def finderrors(prompt, userinput): #function to find errors
     return errors, correctentries
 
         
-
-
 def elapsedtime(start, end): #function to find elapsed time
     time = (end - start)
 
@@ -60,22 +77,80 @@ def typingspeed(userinput, errors, time): #function to find typing speed
 
 def typingaccuracy(correctentries, prompt): #function to find percent accuracy
     promptlength = len(prompt.split())
-    accuracy = correctentries/promptlength
+    accuracy = 100*(correctentries/promptlength)
     
     return accuracy
+
+# Code for random word generator
+
+def words():
+    listofwords = ["hello", "computer"]
+
+    x = random.choice(listofwords)
+
+    return x
+
+score = 0
+errors = 0
+
+def checkwords(userword, word):
+    global score
+    global errors
+    global my_timer
+
+    if (userword == word):
+        score = score+1
+        print("score: {}".format(score), my_timer, "seconds left")
+    else:
+        errors+=1
+        print("score: {}".format(score), my_timer, "seconds left")
+
+
+def timer():
+    global my_timer
+    my_timer = 60
+    while my_timer > 0:
+        time.sleep(1)
+        my_timer -= 1
+
+def usertype():
+    while my_timer > 0:
+        word = words()
+        print(word)
+        userword = input()
+        checkwords(userword, word)
+    print("IN 60 SECONDS\nFinal Score: {}\nErrors: {}\n".format(score, errors))
+
+
     
 
 if __name__ == '__main__':
-    prompt = prompts()
-    input("To start, press enter: ")
-    print(prompt)
-    start = time.time()
-    userinput = input()
-    end = time.time()
+    thread1 = threading.Thread(target=timer)
+    thread2 = threading.Thread(target=usertype)
+    thread1.start()
+    thread2.start()
+    # input("TO START, PRESS ENTER\n")
+    # gamemode = input("Type SpeedTest to play typing speed test game\
+    # \nType TimedWord to play timed typing word game\n:  ")
 
-    usertime = elapsedtime(start, end)
-    errors, correctentries = finderrors(prompt, userinput)
-    netwpm, grosswpm = typingspeed(userinput, errors, usertime)
-    accuracy = typingaccuracy(correctentries, prompt)
-    
-    print("You had {} errors\nAccuracy: {}%\nNetWPM: {}\nGrossWPM: {}\nTime: {}".format(errors, accuracy, netwpm, grosswpm, usertime))
+    # if (gamemode == "SpeedTest"):
+    #     prompt = prompts()
+    #     print(prompt)
+    #     start = time.time()
+    #     userinput = input()
+    #     end = time.time()
+
+    #     usertime = elapsedtime(start, end)
+    #     errors, correctentries = finderrors(prompt, userinput)
+    #     netwpm, grosswpm = typingspeed(userinput, errors, usertime)
+    #     accuracy = typingaccuracy(correctentries, prompt)
+        
+        
+    #     print("You had {} errors\nAccuracy: {}%\nNetWPM: {}\nGrossWPM: {}\nTime: {}".format(errors, accuracy, netwpm, grosswpm, usertime))
+    # elif (gamemode == "TimedWord"):
+    #     pass
+    #     word = words()
+
+
+    # else:
+    #     print("That is not an input, try again")
