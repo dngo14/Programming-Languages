68a57a486295b88350b6eff32d5bf71cebf1ed96
Daniel <ngo8@stolaf.edu>
2021-10-10 23:51:31 -0500

    hw 12

---
+++ hw12/hw12.scm
@@ -0,0 +1,47 @@
+"hello"
+42
+(+ 2 2)
+(* -12 10)
+(a b c d)
+(car '(a b c))
+(cons 'a '(b c))
+
+(define square
+  (lambda (n)
+    (* n n)))
+
+(square 0.5
+
+(+ 1/2 1/2)
+
+(+ (+ 2 2) (+ 2 2))
+
+(- 2 (* 4 1/3))
+
+(quote (1 2 3 4 5))
+
+(quote ("this" "is" "a" "list"))
+
+(quote (+ 3 4))
+
+(quote ("this" "is" "a" "list"))
+
+'(1 2 3 4)
+
+'((1 2) (3 4))
+
+'(/ (* 2 -1) 3) 
+
+(cdr '(a b c))
+
+(car '((a b) (c d)))
+
+(cons 'a '())
+
+(cons 'a (cons 'b (cons 'c '())))
+
+(car (cons 'a '(b c)))
+
+(cons 'a 'b)
+
+(cons 'a '(b . c))
