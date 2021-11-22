e654086e08e797aad4089cf677ee64f911a572c4
Daniel <ngo8@stolaf.edu>
2021-11-22 01:32:26 -0600

    mostly completely hw28

---
+++ hw28/hw28.js
@@ -0,0 +1,85 @@
+#!/usr/bin/node
+
+let output = ""
+
+// const parser = (string) => {
+//     var match = /\r|\n/.exec(string)
+//     let lines
+//     if (match) {
+//         lines = string.split("\n")
+//     }else {
+//         lines = string
+//     }
+//     for (let i = 0; i < lines.length; i+=1) {
+//         let words = lines[i].split()
+//         if (words[0] == "c") {
+//             "turtle.color(words[1], words[2])"
+//         }else if (words[0][0] == "f"){
+//             "turtle.fd(parseInt(words[0]))"
+//         }else if (words[0][0] == "b"){
+//             "turtle.bk(parseInt(words[0]))"
+//         }else if (words[0][0] == "l"){
+//             "turtle.lt(parseInt(words[0]))"
+//         }else if (words[0][0] == "r"){
+//             "turtle.rt(parseInt(words[0]))"
+//         }else if(words[1] == "repeat") {
+//             for (let count = 0; count < parseInt(words[2]); i+=1) {
+                
+//             }
+//         }else if(words[0] == "fill") {
+//             turtle.begin_fill()
+//             let line2 = lines[i].match(/{([^}]*)}/)
+//             parser(line2)
+//         }
+//     }
+// }
+
+let prettystring = ""
+const parser = (string) => {
+    if (string.includes("\n")) {
+            lines = string.split("\n")
+    }
+        else {
+            lines = string
+        }
+    let updatedline = string
+        if (lines[0].slice(0,2) == "c ") {
+            let words = lines[0].split(" ")
+            //turtle.color(words[1], words[2])
+            prettystring += "color\n"+words[1]+"\n"+words[2]+"\n"
+            updatedline = updatedline.replace("c ", "");
+            updatedline = updatedline.replace(words[1]+" ", "");
+            updatedline = updatedline.replace(words[2]+"\n", "");
+            parser(updatedline)
+        }else if (lines[0].slice(0,7) == "fill { ") {
+            //turtle.begin_fill()
+            updatedline = updatedline.replace("fill { ", "")
+            prettystring+="Fill Begin\n"
+            parser(updatedline)
+            prettystring+="Fill End\n"
+        }else if(lines[0].slice(0,6) == "repeat") {
+            updatedline = lines.slice(1, lines.length)
+            let words = lines[0].split(" ")
+            let reps = parseInt(words[1])
+            prettystring+="Repeat "+words[1]+"\n"
+            for (let i = 0; i < reps; i+=1) {
+                parser(updatedline)
+            }
+            prettystring+="\n"
+            //console.log(updatedline)
+        } else if(lines[0].includes("f") || lines[0].includes("b") || lines[0].includes("l") || lines[0].includes("r") && lines[0].matches(".*\\d.*")) {
+            let words = lines[0].split()
+            for (let i = 0; i < words.length; i+=1) {
+                prettystring+=words[i].replace("}}", "")+" "
+            }
+        }
+    return updatedline
+}
+
+let x = "c green blue\nfill { repeat 36 {\nf200 l170}}"
+let y = x.match(/{([^}]*)}/)[0]
+//console.log(y.match(/[^{\}]+(?=})/g))
+//console.log(x.split("\n"))
+let z = parser("c green blue\nfill { repeat 36 {\nf200 l170}}")
+//console.log(z)
+console.log(prettystring)
